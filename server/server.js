dca6e95fc18dd4fcb32b521f844ff8aaf34bc1cf
Darshan <darshanpatel2923@gmail.com>
2024-03-30 20:19:03 +0530

    did a little changes in connection

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ app.use(cors(corsConfig));
 
 const port = process.env.PORT || 8700
 
+mongoose.connect("mongodb://0.0.1.27010/cruddb")
+
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => console.log("Server running successfully"));
